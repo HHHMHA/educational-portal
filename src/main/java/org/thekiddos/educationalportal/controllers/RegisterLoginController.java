2b2bcbd711a50f979b152e837dee3707530fbbb5
J2mF <mp4-12cs5@hotmail.com>
2021-06-11 12:53:16 +0300

    Add register-login view

--- src/main/java/org/thekiddos/educationalportal/controllers/RegisterLoginController.java
+++ src/main/java/org/thekiddos/educationalportal/controllers/RegisterLoginController.java
@@ -1,9 +1,26 @@
 package org.thekiddos.educationalportal.controllers;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleGroup;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView( "register-login.fxml" )
 public class RegisterLoginController {
+    public JFXTextField nameField;
+    public JFXPasswordField passwordField;
+    public JFXTextField registerNameField;
+    public JFXPasswordField registerPasswordField;
+    public ToggleGroup userType;
+
+    public void login( ActionEvent event ) {
+
+    }
+
+    public void register( ActionEvent event ) {
+
+    }
 }
